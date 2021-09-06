06bd6e0675427157a66e7022f2554b7e942c4f7d
Charlotte Schack Berg <csbe@itu.dk>
2021-09-06 16:20:32 +0200

    Tested and implemented IsLeapYear

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -6,9 +6,45 @@ namespace MyApp.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void isLeapYearTrue_Divisible_by_4()
         {
+            var program = new Program();
+            int year1 = 1444;
 
+            var output = program.isLeapYear(year1);
+            Assert.True(output);
+        }
+
+        [Fact]
+        public void isLeapYearFalse_Divisible_by_4()
+        {
+            var notLeapYearProgram = new Program();
+            int year2 = 1333;
+
+            var output = notLeapYearProgram.isLeapYear(year2);
+            Assert.False(output);
+        }
+
+        [Fact]
+        public void LeapYear_Divisble_by_100()
+        {
+            var notLeapYear100 = new Program();
+            var year_divisble_100 = 1700;
+
+            var output = notLeapYear100.isLeapYear(year_divisble_100);
+            Assert.False(output);
+            
+        }
+
+
+        [Fact]
+        public void LeapYear_Divisible_By_400()
+        {
+            var leapYear400 = new Program();
+            int year_Divisible_400 = 40_000;
+
+            var output = leapYear400.isLeapYear(year_Divisible_400);
+            Assert.True(output);
         }
     }
 }
--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -2,11 +2,36 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        private bool leapYear;
+
+        public Program(){
+            leapYear = false;
         }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("result: ");
+        }
+
+        public bool isLeapYear(int year)
+    {
+        if(year % 4 == 0)
+        {
+            if(year % 100 == 0)
+            {
+                leapYear = false;
+                if(year % 400 == 0){
+                    leapYear = true;
+                }
+            } else 
+            {
+                leapYear = true;
+            }
+        }
+        return leapYear;
+    }
     }
 }
